031282d4a6111953108dc6a7a6da01f76ed93f93
Suat Can Ölçer <132690977+GriffithsofBerserk@users.noreply.github.com>
2023-08-05 00:17:29 +0300

    first commit ( Kodluyoruz )

---
+++ src/main/java/com/patika/girisspringboot/GirisSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.patika.girisspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GirisSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GirisSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/patika/girisspringboot/GirisSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patika.girisspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GirisSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
