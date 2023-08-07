e4c559546d407f605acede3ecdcd322b858e4859
Suat Can Ölçer <132690977+GriffithsofBerserk@users.noreply.github.com>
2023-08-07 22:28:32 +0300

    kısayollar

---
+++ src/main/java/com/patika/utils/IntellijShortCut.java
@@ -0,0 +1,9 @@
+package com.patika.utils;
+
+public class IntellijShortCut {
+    //Setting ==> ctrl+alt+s
+    //Alt+Ins ==> constructor, toString, getter setter
+    // Ctrl+/ => single comment
+    //Ctrl+shift+/ ==> multiple comment
+
+}
