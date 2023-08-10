b3a0b1ba354b2ad055ce123492f2fc3bf67cf09e
Suat Can Ölçer <132690977+GriffithsofBerserk@users.noreply.github.com>
2023-08-11 00:14:42 +0300

    messages.properties

--- src/main/java/com/patika/controller/ThymeleafController.java
+++ src/main/java/com/patika/controller/ThymeleafController.java
@@ -31,5 +31,12 @@ public class ThymeleafController {
         model.addAttribute("key_model2","Ben modelden geldim 2");
         return "thymeleaf_file/thymeleaf3";
     }
+    //url http://localhost:8080/thymeleaf4
+    @GetMapping("/thymeleaf4") //oluşacak sayfanın urlsinin devamını belirtmesi için kullanıyoruz.
+    //@ResponseBody
+    public String getThymeleaf4Model(Model model){
+        model.addAttribute("key_model1","Ben modelden geldim-4");
+        return "thymeleaf4";
+    }
 
 }
